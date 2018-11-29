fad320cb04e29e202ad3862cf31523a4e0968fdd
Rachel Winkler <rachelwinkler9@gmail.com>
2018-11-29 13:35:31 +0000

    completed more exercises from ch02

--- ch02/ch02_rachel_exercises.py
+++ ch02/ch02_rachel_exercises.py
@@ -53,3 +53,41 @@ print(f)
 name="Sarah"
 print(f)
 print(f*5)
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+#A comment, this is so you can read your program later.
+#Anything after the # is ignored by python.
+print("I could have code like this.") # and the comment after is ignored
+#You can also use a comment to "disable" or a comment out a piece of code:
+#print "this won't run."
+print("This will run.")
+
+print("I will now count my chicken:")
+
+print("Hens", 25+30/6)
+print("Roosters", 100-25*3%4)
+
+print("Now I will count the eggs:")
+      
+print(3_2_1-5+4%2-1/4+6)
+
+print("Is it true that 3+2<5-7?")
+
+print(3+2<5-7)
+
+print("what is 3+2?",3+2)
+print("What is 5-7?", 5-7)
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater", 5>-2)
+print("Is it greater or qual?", 5>=-2)
+print("Is it less or equal?", 5<=-2)
