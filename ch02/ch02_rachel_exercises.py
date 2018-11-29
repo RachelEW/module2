d2fd8b8b0fa497e3bc76e3c89204ef666452d4cc
Rachel Winkler <rachelwinkler9@gmail.com>
2018-11-29 17:30:52 +0000

    more exercises completed

--- ch02/ch02_rachel_exercises.py
+++ ch02/ch02_rachel_exercises.py
@@ -107,3 +107,104 @@ print ("There will be", cars_not_driven, "empty cars today.")
 print ("We can transport", carpool_capacity, "people today.")
 print ("We have", passengers, "to carpool today.")
 print ("We need to put about", average_passengers_per_car, "in each car")
+
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print ("Let's talk about %s" % my_name)
+print ("He's %d inches tall." % my_height)
+print ("He's %d pounds heavy." % my_weight)
+print ("Actually that's not too heavy.")
+print ("He's got %s eyes and %s hair." % (my_eyes, my_hair))
+print ("His teeth are usually %s depending on the coffee." % my_teeth)
+
+print ("If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
+
+x = "there are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+print (x)
+print (y)
+
+print ("I said: %r." % x)
+print ("I also said: '%s'." %y)
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print (joke_evaluation % hilarious)
+
+w = ("This is the left side of...")
+e = ("a string with a right side.")
+
+print (w + e)
+
+print ("Mary had a little lamb.")
+print ("Its fleece was white as %s." % 'snow')
+print ("And everywhere that Mary went.")
+print ("." * 10)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print (end1 + end2 + end3 + end4 + end5 + end6,
+end7 + end8 + end9 + end10 + end11 + end12)
+
+formatter = "%r %r %r %r"
+
+print (formatter % (1, 2, 3, 4))
+print (formatter % ("one", "two", "three", "four"))
+print (formatter % (True, False, False, True))
+print (formatter % (formatter, formatter, formatter, formatter))
+print (formatter % (
+        "I had this thing.",
+        "That you could type up right.",
+        "But it didn't sing.",
+        "So I said goodnight."
+        ))
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print ("Here are the days: ", days)
+print ("Here are the months: ", months)
+
+print ("""
+       There's something going on here.
+       With the three double-quotes.
+       We'll be able to type as much as we like.
+       Even 4 lines if we want, or 5, or 6.
+       """)
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat ="I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* fishies
+\t* Catnip\n\t* Grass
+"""
+    
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
--- ch03/ch03_rachel_in_class.py
+++ ch03/ch03_rachel_in_class.py
@@ -5,32 +5,32 @@ Created on Thu Nov 29 13:54:55 2018
 @author: 612383461
 """
 
-#print ("What's your name?")
-#name = input()
-#
-#print ("Hello {}!".format((name).upper()))
-#
-##The first option gives a space between the question and where you type
-#
-#name = input("What's your name? ").title()
-#print ("Hello {}!".format(name.upper()))
+print ("What's your name?")
+name = input()
+
+print ("Hello {}!".format((name).upper()))
+
+#The first option gives a space between the question and where you type
+
+name = input("What's your name? ").title()
+print ("Hello {}!".format(name.upper()))
 
 #The second option gives the input o the same line as the question
 
-#print ("Where are you from?")
-#hometown = input()
-#
-#print ("Oooh, I loved {} when I visited!".format((hometown).title()))
-#
-#print ("How old are you?")
-#age = input()
-#
-#print ("{} is a great age, {}!".format(age, (name.title())))
-#
-#print ("What colour are your socks?")
-#socks = input().title()
-#
-#print ("{}, I like your sense of style, {}.".format(socks,name))
+print ("Where are you from?")
+hometown = input()
+
+print ("Oooh, I loved {} when I visited!".format((hometown).title()))
+
+print ("How old are you?")
+age = input()
+
+print ("{} is a great age, {}!".format(age, (name.title())))
+
+print ("What colour are your socks?")
+socks = input().title()
+
+print ("{}, I like your sense of style, {}.".format(socks,name))
 
 def hello_world():
     print ("Hello World!")
@@ -62,7 +62,7 @@ def adding_numbers():
     print ("Choose a second number")
     number_two = input()
     result = float(number_one)+float(number_two)
-    print ("The sum of the numbers you chose is {}.".format(result))
+    print ("The sum of the numbers you choose is {}.".format(result))
     
 adding_numbers()
 
