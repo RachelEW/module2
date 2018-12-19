7f1bbfcfdc743d077a22e749c97920597709b3b0
Rachel Winkler <rachelwinkler9@gmail.com>
2018-12-19 16:38:28 +0000

    updated ch07

--- ch07_debugging/ch07_rachel_debugging.py
+++ ch07_debugging/ch07_rachel_debugging.py
@@ -1,9 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec 11 09:14:36 2018
+Created on Wed Dec 19 12:20:09 2018
 
-@author: 612383461
+@author: winkl
 """
+
+###############################################
+"""Debugging Using Print to Check for Errors"""
+###############################################
+
+#Original code to debug:
+
+userInput = input('please give a number ')
+result = userInput - 2
+
+#The original code gives a TypeError, therefore print the type of userInput to find out what the error is
+#This shows userInput is a string
+#To make it work convert userInput into an integer
+
+userInput = input('please give a number ')
+#print(type(userInput))
+userInput = int(userInput)
+result = userInput - 2
+
+#Always remove debugging lines after!
 
 ################################################
 """Debugging using Python's Debugging System"""
