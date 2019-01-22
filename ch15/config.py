5eb2159096f0368ca6c772ade887c66cfafca6f8
Rachel Winkler <rachelwinkler9@gmail.com>
2019-01-22 15:22:34 +0000

    updating files for github

--- ch15/config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 22 14:59:21 2019
-
-@author: winkl
-"""
-
-weather_api_key = "7ca7b4bc86f2a2b157f51a4351e99078"
-mailgun_api_key = "d67c0b8b74ab329ec42955a0cc724e39-060550c6-23bf1e35"
